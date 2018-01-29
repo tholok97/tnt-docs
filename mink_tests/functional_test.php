274f5df3edff614b0d6582db8375a890669fefee
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2018-01-29 13:50:48 +0100

    commentified mink test

--- mink_tests/functional_test.php
+++ mink_tests/functional_test.php
@@ -2,11 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 
-
 require_once 'vendor/autoload.php';
-
-
-
 
 class BookfaceTest extends TestCase {
 
@@ -20,6 +16,7 @@ class BookfaceTest extends TestCase {
         $session->visit("http://10.212.136.82/");
         $page = $session->getPage();
 
+        // header works if includes the text 'bookface'
         $this->assertEquals(
             'bookface',
             $page->find('css', '.title')->getText(),
@@ -37,6 +34,8 @@ class BookfaceTest extends TestCase {
         $session->visit("http://10.212.136.82/");
         $page = $session->getPage();
 
+        // basic db test: Latest activity seems to only show up when db 
+        // is working
         $this->assertEquals(
             'Latest activity',
             $page->find('css', 'h2')->getText(),
